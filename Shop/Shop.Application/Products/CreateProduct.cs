351261b798e4172bb32564fcc91003fb11e09afd
Geraldy <geraldy.miero@gmail.com>
2020-07-02 20:52:20 +0700

    Add DB Context

--- Shop/Shop.Application/Products/CreateProduct.cs
+++ Shop/Shop.Application/Products/CreateProduct.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using Shop.Database;
+using Shop.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Shop.Application.Products
 {
-    class CreateProduct
+    public class CreateProduct
     {
+        private ApplicationDBContext _context;
+        public CreateProduct(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        public void Do(int id, string Name, string Description)
+        {
+            _context.Products.Add(new Product
+            {
+                Id = id,
+                Name = Name,
+                Description = Description
+            });
+        }
     }
 }
---
+++ Shop/Shop.Database/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Shop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.Database
+{
+    public class ApplicationDBContext : IdentityDbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+            :base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
